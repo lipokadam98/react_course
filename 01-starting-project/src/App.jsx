dd33d337dfe5b145210cbe0bd4c9286dfc8b4c2a
Adam Lipok <lipokadam98@gmail.com>
2025-03-19 22:47:38 +0100

    State management and conditional rendering

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,27 @@
+import {useState} from 'react';
 import {CORE_CONCEPTS} from "./data";
+import {EXAMPLES} from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState();
 
     function handleSelect(selectedButton) {
-        console.log(selectedButton);
+        setSelectedTopic(selectedButton);
+    }
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent =
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
     }
 
     return (
@@ -42,7 +57,7 @@ function App() {
                             State
                         </TabButton>
                     </menu>
-                    Dynamic Content
+                    {tabContent}
                 </section>
             </main>
         </div>
