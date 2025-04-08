7b7db25570e0d453b6c4135c5818d7e0ce899598
Adam Lipok <lipokadam98@gmail.com>
2025-04-08 22:32:57 +0200

    Correct way of updating the state

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,17 +4,22 @@ export default function Player({name, symbol}) {
     const [isEditing, setIsEditing] = useState(false)
 
     function setIsEditingValue(){
-        setIsEditing(true);
+        //It is important to use a function when updating states, because
+        //it will use the latest value from the state
+        setIsEditing(editing => !editing);
     }
+
+    let btnCaption = isEditing ? 'Edit' : 'Save';
+
     return  <li>
                   <span className="player">
                       {
                           isEditing ?
-                          <input className='player-name' value={name}></input> :
+                          <input type='text' required value={name} /> :
                           <span className='player-name'>{name}</span>
                       }
                       <span className='player-symbol'>{symbol}</span>
                   </span>
-                <button onClick={setIsEditingValue}>Edit</button>
+                <button onClick={setIsEditingValue}>{btnCaption}</button>
             </li>
 }
