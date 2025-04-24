33ec46a7363333104229c8271a30559865f15b04
Adam Lipok <lipokadam98@gmail.com>
2025-04-25 00:20:51 +0200

    Styled components finished

--- styling-starting-project/src/components/AuthInputs.jsx
+++ styling-starting-project/src/components/AuthInputs.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {styled} from 'styled-components';
 import {Button} from "./Button.jsx";
+import CustomInput from "./Input.jsx";
 
 const ControlContainer = styled.div`
   display: flex;
@@ -8,28 +9,6 @@ const ControlContainer = styled.div`
   gap: 0.5rem;
   margin-bottom: 1.5rem;
 `
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  color: ${({$invalid})=> $invalid ? '#ef4444' : '#374151'};
-  background-color: ${({$invalid})=> $invalid ? '#fed2d2' : '#d1d5db'};
-  border: 1px solid ${({$invalid})=> $invalid ? '#f73f3f' : 'transparent'};
-`
-
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -55,24 +34,22 @@ export default function AuthInputs() {
 
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
+
+          <CustomInput
+            label='Email'
             type="email"
-            $invalid={emailNotValid}
+            invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
+
+          <CustomInput
+            label='Password'
             type="password"
-            $invalid={passwordNotValid}
+            invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
