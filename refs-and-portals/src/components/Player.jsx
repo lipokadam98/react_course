fec676121b37df645ab200e73f940ac7c0a5ca30
lipoka <lipokadam98@gmail.com>
2025-05-03 21:47:59 +0200

    Using refs in react

--- refs-and-portals/src/components/Player.jsx
+++ refs-and-portals/src/components/Player.jsx
@@ -1,24 +1,20 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function Player() {
-    const [enteredPlayerName, setEnteredPlayerName] = useState('');
-    const [submitted, setSubmitted] = useState(false);
 
-    function handleChange(event) {
-        setSubmitted(false)
-        const value = event.target.value;
-        setEnteredPlayerName(value);
-    }
+    const playerName = useRef();
+
+    const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
     function handleClick() {
-        setSubmitted(true);
+        setEnteredPlayerName(playerName.current.value);
     }
 
     return (
     <section id="player">
-      <h2>Welcome {submitted? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+        <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
