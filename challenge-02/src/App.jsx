a1a6a955992d9f333cd5e121d6b1881dfba7a932
lipoka <lipokadam98@gmail.com>
2025-05-10 23:57:44 +0200

    Creating the sidebar component template

--- challenge-02/src/App.jsx
+++ challenge-02/src/App.jsx
@@ -1,6 +1,9 @@
+import Sidebar from "./components/Sidebar.jsx";
+
 function App() {
   return (
     <>
+        <Sidebar></Sidebar>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
---
+++ challenge-02/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+export default function Sidebar(){
+    return <section className="fixed w-64 rounded-r-2xl bg-zinc-900 h-full pt-16 pl-6 pr-4">
+            <h1 className="text-teal-50 text-xl font-bold uppercase mb-8">
+                Your projects
+            </h1>
+            <button className="text-slate-300 rounded pt-2 pb-2 pl-4 pr-4 bg-stone-700 hover:bg-stone-500 mb-8">
+                + Add Project
+            </button>
+            <ul className="text-teal-50">
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+                <li>Project 4</li>
+                <li>Project 5</li>
+            </ul>
+        </section>
+}
