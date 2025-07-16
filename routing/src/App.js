ab01f8b6d39e89b6a89c3d029a790b972214c39a
lipoka <lipokadam98@gmail.com>
2025-07-16 15:44:08 +0200

    More basic routing features in react

--- routing/src/App.js
+++ routing/src/App.js
@@ -3,13 +3,15 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import RootLayout from "./pages/RootLayout";
 import Error from "./pages/Error";
+import ProductDetail from "./pages/ProductDetail";
 
 const router = createBrowserRouter([
   {
     path: '/', element: <RootLayout />, errorElement: <Error />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/products', element: <Products /> },
+      { element: <Home /> ,index: true },
+      { path: 'products', element: <Products /> },
+      { path: 'products/:productId', element: <ProductDetail /> },
     ]
   },
 ])
--- routing/src/pages/Home.jsx
+++ routing/src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import {Link} from "react-router-dom";
+import {Link,useNavigate} from "react-router-dom";
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
+    function navigateHandler(){
+        navigate('products');
+    }
+
     return <>
         <h1>Home</h1>
         <p>Welcome to the home page</p>
-        <p>Go to <Link to='/products'>the list of products</Link></p>
+        <p>Go to <Link to='products'>the list of products</Link></p>
+        <p>
+            <button onClick={navigateHandler}>Navigate</button>
+        </p>
     </>;
 }
