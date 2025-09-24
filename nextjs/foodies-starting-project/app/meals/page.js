a94b108362461e60e103fe5293dc7ab496cf4da2
lipoka <lipokadam98@gmail.com>
2025-09-24 14:58:01 +0200

    Adding more components and using the use client

--- nextjs/foodies-starting-project/app/meals/page.js
+++ nextjs/foodies-starting-project/app/meals/page.js
@@ -1,18 +1,23 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
-    return <section>
-        <h1>Meals works</h1>
-        <ul>
-            <li>
-                <Link href="meals/1">Meal 1</Link>
-            </li>
-            <li>
-                <Link href="meals/2">Meal 2</Link>
-            </li>
-            <li>
-                <Link href="meals/2">Meal 3</Link>
-            </li>
-        </ul>
-    </section>
+    return <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created {' '}
+                <span className={classes.highlight}>by you</span>
+            </h1>
+            <p>
+                Choose your favorite recipe and cook it yourself. It is easy and fun
+            </p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">Share Your Favorite Recipe</Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]}/>
+        </main>
+    </>
 }
